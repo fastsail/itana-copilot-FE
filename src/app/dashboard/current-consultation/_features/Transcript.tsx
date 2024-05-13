890ee47603af99d01f79a539fe721ed3328cbd40
by.segunMoses <mosesdave97372@yahoo.com>
2024-05-13 18:02:49 +0100

    api axios generate from template

--- src/app/dashboard/current-consultation/_features/Transcript.tsx
+++ src/app/dashboard/current-consultation/_features/Transcript.tsx
@@ -189,7 +189,7 @@ const Transcript = () => {
 
         try {
             // Use the provided template
-            const response = await axios.get(`/api/llama_api?user_message=${template}&max_tokens=${maxTokens}`);
+            const response = await axios.get(`https://llama-api-image-sbmzuuqa7a-uc.a.run.app/llama?user_message=${template}&max_tokens=${maxTokens}`);
             console.log('data:', response.data);
             const processedData = processChunks(response.data);
             setCompletedNoteGenerations(processedData);
